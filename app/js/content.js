03f2cef6db1b9b85d3bb3d83a4191701570b73a6
Tony Jian <tonytonyjan@gmail.com>
2012-08-17 01:15:53 +0800

    rm

--- app/js/content.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("dblclick", function(event){
-  if(event.ctrlKey){
-    var q = window.getSelection().toString();
-    chrome.extension.sendMessage({op: "query", q:q});
-  }
-});
