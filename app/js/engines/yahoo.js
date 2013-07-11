565e779c6f9a694ac6f58e60437f3fac93f55c11
Tony Jian <tonytonyjan@gmail.com>
2013-07-11 14:34:15 +0800

    fix #17

--- app/js/engines/yahoo.js
+++ app/js/engines/yahoo.js
@@ -1,6 +1,6 @@
 TJDict.engines.push({
   title: "Yahoo 字典",
-  url: function(q){return "http://tw.dictionary.yahoo.com/dictionary?p=" + q},
+  url: function(q){return "http://tw.dictionary.search.yahoo.com/search?p=" + q},
   resault: function(data){
     var body = $(TJDict.grepBody(data));
     return body.find('.explanation_wrapper').html();
