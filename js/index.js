90fbbeec48395ffe1cfd0edd3f0b19cfa09c5b9c
Tony Jian <tonytonyjan@gmail.com>
2014-08-11 02:33:10 +0800

    add modal

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $('[data-toggle="tooltip"]').tooltip();
 (function(){
+  // query
   var queryString = urlParams.q ? urlParams.q.trim() : '';
   $('#q').val(queryString).focus();
   for(var i in DICTIONARIES){
+    $('#dict-list').append('<li>' + DICTIONARIES[i].title + '</li>');
     DICTIONARIES[i].query(queryString, function(dictionary, result){
       $('#main').append('<div class="page-header"><h2>' + dictionary.title + '</h2></div>');
       $('#main').append(result);
