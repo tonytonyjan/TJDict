6a86d420f4108c54747f0df76f4adf093c16c651
Tony Jian <tonytonyjan@gmail.com>
2014-08-11 13:39:39 +0800

    get setting

--- js/index.js
+++ js/index.js
@@ -4,15 +4,18 @@ $('[data-toggle="tooltip"]').tooltip();
   var queryString = urlParams.q ? urlParams.q.trim() : '';
   $('#q').val(queryString).focus();
   if(queryString)
-    for(var i in DICTIONARIES){
-      // query
-      DICTIONARIES[i].query(queryString, function(dictionary, result){
-        $('#main').append('<div class="page-header"><h2>' + dictionary.title + '</h2></div>');
-        $('#main').append(result);
-      });
-      // #modal_about
-      $('#dict-list').append('<li>' + DICTIONARIES[i].title + '</li>');
-      // #modal_setting
-      $('#dict_checkboxes').append('<div class="checkbox"><label><input id="' + i + '" name="' + i + '" type="checkbox"> ' + DICTIONARIES[i].title + '</label></div>');
-    }
+    chrome.storage.sync.get(null, function(items){
+      for(var i in items)
+        if(DICTIONARIES[i] && items[i])
+          DICTIONARIES[i].query(queryString, function(dictionary, result){
+            $('#main').append('<div class="page-header"><h2>' + dictionary.title + '</h2></div>');
+            $('#main').append(result);
+          });
+    });
+  for(var i in DICTIONARIES){
+    // #modal_about
+    $('#dict-list').append('<li>' + DICTIONARIES[i].title + '</li>');
+    // #modal_setting
+    $('#dict_checkboxes').append('<div class="checkbox"><label><input id="' + i + '" name="' + i + '" type="checkbox"> ' + DICTIONARIES[i].title + '</label></div>');
+  }
 })();
