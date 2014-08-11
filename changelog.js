282944b5f31f481004e738781791ce464ff8a0c1
Tony Jian <tonytonyjan@gmail.com>
2014-08-11 21:08:49 +0800

    update changelog

--- changelog.js
+++ changelog.js
@@ -3,8 +3,8 @@ var CHANGELOG = {
     items: [
       {title: '更新', message: '架構全面改寫，使用 Chrome 最新 API'},
       {title: '更新', message: '筆者終於買 Mac 了，怒支援 Mac'},
-      {title: '異動', message: '移除日文字典'},
-      {title: '異動', message: '移除右鍵選單'},
+      {title: '更新', message: '字典顯示排序功能'},
+      {title: '異動', message: '移除日文字典、右鍵選單'},
       {title: '修正', message: '彈出視窗的顯示'}
     ]
   }
