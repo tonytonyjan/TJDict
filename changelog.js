40cf41f81b9b878b246cf34e7b4c1aa4bd4fddfe
Tony Jian <tonytonyjan@gmail.com>
2014-08-13 02:15:49 +0800

    update changelog

--- changelog.js
+++ changelog.js
@@ -1,3 +1,6 @@
 var CHANGELOG = [
-  {title: '更新', message: '新增字典：Urban Dictionary'}
+  {title: '更新', message: '新增字典：Urban Dictionary'},
+  {title: '更新', message: '新增 Browser Action，可單獨使用'},
+  {title: '更新', message: '新增首頁'},
+  {title: '修正', message: 'Window 上的組合鍵問題'}
 ]
