ac96793ac8d795fc7e0bf31a67a6ee5ff8e3637f
Tony Jian <tonytonyjan@gmail.com>
2014-08-12 18:16:04 +0800

    add jp dictionary

---
+++ js/dictionaries/weblio.jp.js
@@ -0,0 +1,23 @@
+DICTIONARIES.weblio = {
+  title: "Weblio（日文）",
+  query: function(q, response){
+    var self = this;
+    $.get("http://www.weblio.jp/content/" + q).done(function(data){
+      var titles = $(data).find('.pbarTL');
+      var contents = $(data).find('.kiji');
+      var minSize = titles.size() < contents.size ? titles.size() : contents.size();
+      var reg = /www\.weblio\.jp\/content\/(.*)/;
+      $(contents).find('a').each(function(key, value){
+        var match = reg.exec(value.href);
+        if(match)
+          value.href = 'index.html?q=' + match[1];
+      });
+      var result = ""
+      for(var i = 0; i < minSize; i++){
+        result += '<h3>' + titles[i].innerText + '</h3>';
+        result += contents[i].innerHTML;
+      }
+      response(self, result);
+    });
+  }
+};
