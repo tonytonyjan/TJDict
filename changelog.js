ca01c3d49174b06edc4c3f5761d7275d7674f284
Tony Jian <tonytonyjan@gmail.com>
2014-08-12 15:11:27 +0800

    bump version

--- changelog.js
+++ changelog.js
@@ -1,4 +1,13 @@
 var CHANGELOG = {
+  '1.0.1': {
+    items: [
+      {title: '修正', message: '嘗試修正 Window 下閃退問題'},
+      {title: '其他', message: '架構更新後筆者還在搬其餘字典，包括'},
+      {title: '', message:     '日文，造成不便還請見諒。筆者會努力讓軟'},
+      {title: '', message:     '體更便於使用，也非常感謝大家給予任何'},
+      {title: '', message:     '意見。:")'}
+    ]
+  },
   '1.0.0': {
     items: [
       {title: '更新', message: '筆者終於買 Mac 了，怒支援 Mac'},
