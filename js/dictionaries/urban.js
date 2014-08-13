6a9fa818c6ea20fb6f6d6f5e139f55d7a2a9339f
Tony Jian <tonytonyjan@gmail.com>
2014-08-13 12:45:40 +0800

    remove console log

--- js/dictionaries/urban.js
+++ js/dictionaries/urban.js
@@ -6,11 +6,9 @@ DICTIONARIES.urban = {
     $.get('http://www.urbandictionary.com/define.php?term=' + q).done(function(data){
       var result = '';
       $(data).find('.box > div.inner').each(function(index, element){
-        console.log(element);
         var word = $(element).find('.word').text();
         var meaning = $(element).find('.meaning').html();
         var example = $(element).find('.example').html();
-        console.log(word);
         result += '<div class="panel panel-info">\
           <div class="panel-heading">\
             <h3 class="panel-title">' + word + '</h3>\
