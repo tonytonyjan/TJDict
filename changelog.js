e1a6fb7aa2625bab41a034581beceeabf9f39e9d
Tony Jian <tonytonyjan@gmail.com>
2014-08-16 02:58:11 +0800

    update changelog

--- changelog.js
+++ changelog.js
@@ -1,3 +1,5 @@
 var CHANGELOG = [
-  {title: '修正', message: '字典導覽頁面出現在首頁'}
+  {title: '修正', message: '字典導覽頁面出現在首頁'},
+  {title: '修正', message: '會出現沒有搜尋結果的字典'},
+  {title: '修正', message: '字典導覽列會蓋到發音下拉選單'}
 ]
