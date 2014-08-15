2cfa6685657e26435be50d0443908580cf82f76c
Tony Jian <tonytonyjan@gmail.com>
2014-08-16 03:42:46 +0800

    updat changelog

--- changelog.js
+++ changelog.js
@@ -1,4 +1,5 @@
 var CHANGELOG = [
+  {title: '異動', message: '改變分頁大小不會影響彈出視窗大小'},
   {title: '修正', message: '字典導覽頁面出現在首頁'},
   {title: '修正', message: '會出現沒有搜尋結果的字典'},
   {title: '修正', message: '字典導覽列會蓋到發音下拉選單'}
