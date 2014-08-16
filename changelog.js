bd19478916e155fa5b9918a34003e417971bddb7
Tony Jian <tonytonyjan@gmail.com>
2014-08-16 18:50:22 +0800

    bump version

--- changelog.js
+++ changelog.js
@@ -1,6 +1,3 @@
 var CHANGELOG = [
-  {title: '異動', message: '改變分頁大小不會影響彈出視窗大小'},
-  {title: '修正', message: '字典導覽頁面出現在首頁'},
-  {title: '修正', message: '會出現沒有搜尋結果的字典'},
-  {title: '修正', message: '字典導覽列會蓋到發音下拉選單'}
+  {title: '異動', message: '視窗變小的時候只顯示搜尋框，其餘隱藏'}
 ]
