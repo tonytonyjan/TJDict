94d006c38d45da3f982296337762542662e7d8ff
Jian Weihang <tonytonyjan@gmail.com>
2014-11-15 21:25:54 +0800

    - add FAQ - bump to 1.10.1

--- changelog.js
+++ changelog.js
@@ -1,3 +1,6 @@
 var CHANGELOG = [
-  {title: '新增', message: '按住左鍵查詢（預設未啟用）'}
+  {title: '新增', message: '新增「FAQ」按鈕'},
+  {title: '異動', message: '替換當時用小畫家畫的 logo'},
+  {title: '異動', message: '拿掉多餘的「說明」按鈕'},
+  {title: '異動', message: '修飾介紹文字'}
 ]
