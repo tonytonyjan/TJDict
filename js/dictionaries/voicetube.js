5b7e233c364536c97965d395b55b105ab836e18a
Jian Weihang <tonytonyjan@gmail.com>
2015-03-20 21:48:20 +0800

    add vidoe in voicetube

--- js/dictionaries/voicetube.js
+++ js/dictionaries/voicetube.js
@@ -5,7 +5,25 @@ DICTIONARIES.voicetube = {
   query: function(q, response){
     var self = this;
     $.getJSON('https://tw.voicetube.com/definition/' + q, {format: 'json'}, function(data){
-      response(self, data.translate.replace(/<\/a>/g, '').replace(/<a[^>]*>/g, ''));
+      s = '';
+      for(var i in data.videos){
+        var video = data.videos[i];
+        var video_url = 'https://tw.voicetube.com/videos/' + video.info.id;
+        s += '<div class="media"><div class="media-left">' +
+          '<a href="' + video_url + '" target="_blank">' +
+            '<img class="media-object" src="https://cdn.voicetube.com/assets/thumbnails/' + video.info.youtube + '.jpg" height="100px" >' +
+          '</a>' +
+        '</div>' +
+        '<div class="media-body">' +
+          '<h4 class="media-heading">' + video.info.title + '</h4><ol>';
+          for(var j in video.captions){
+            var caption = video.captions[j];
+            var caption_zh = video.captions_zh[j];
+            s += '<li><a href="' + video_url + '/' + video.captions_id[j] + '/?word=' + q + '">' + caption + '</a><br>' + caption_zh + '</li>'
+          }
+        s += '</ol></div></div>'
+      }
+      response(self, data.translate.replace(/<\/a>/g, '').replace(/<a[^>]*>/g, '') + s);
     });
   }
 };
