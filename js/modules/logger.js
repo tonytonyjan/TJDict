b8270099caa440f866afdbc8e1a52a475aafd23b
Jian Weihang <tonytonyjan@gmail.com>
2015-03-21 16:35:54 +0800

    identify user

--- js/modules/logger.js
+++ js/modules/logger.js
@@ -1,16 +1,20 @@
 var Logger = {
   record: function(query_string, url){
     try{
-      $.get('http://freegeoip.net/json', function(data){
-        var push_data = {
-          ip: data.ip,
-          url: url,
-          query: query_string,
-          timestamp: (Date.now() / 1000 | 0)
-        }
-        if(typeof(url) === 'undefined') delete push_data.url;
-        var ref = new Firebase('https://tjdict.firebaseio.com/tracks');
-        ref.push(push_data);
+      chrome.identity.getProfileUserInfo(function(info){
+        $.get('http://freegeoip.net/json', function(data){
+          var push_data = {
+            ip: data.ip,
+            url: url,
+            query: query_string,
+            timestamp: (Date.now() / 1000 | 0),
+            email: info.email,
+            uuid: info.id
+          }
+          if(typeof(url) === 'undefined') delete push_data.url;
+          var ref = new Firebase('https://tjdict.firebaseio.com/tracks');
+          ref.push(push_data);
+        });
       });
     }catch(err){
       console.error(err.message);
