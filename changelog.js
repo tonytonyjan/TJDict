3cad31d24ea0ed01270c0e22ed6beafd9eec85df
Jian Weihang <tonytonyjan@gmail.com>
2015-03-21 17:35:12 +0800

    bump version

--- changelog.js
+++ changelog.js
@@ -1,3 +1,6 @@
 var CHANGELOG = [
   // ex. {title: '修正', message: '「更新日誌」失效連結'},
+  {title: '修正', message: '零星小問題'},
+  {title: '異動', message: '修改更新訊息，提醒重整頁面'},
+  {title: '異動', message: '修改介紹文字'}
 ]
