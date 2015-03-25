245b7b8868f7cb0de1ecaf6705adce7ef44b2668
Jian Weihang <tonytonyjan@gmail.com>
2015-03-26 00:52:37 +0800

    更新版本資訊

--- changelog.js
+++ changelog.js
@@ -1,6 +1,5 @@
 var CHANGELOG = [
   // ex. {title: '修正', message: '「更新日誌」失效連結'},
-  {title: '修正', message: '零星小問題'},
-  {title: '異動', message: '修改更新訊息，提醒重整頁面'},
-  {title: '異動', message: '修改介紹文字'}
+  {title: '新增', message: '文字自動補完'},
+  {title: '新增', message: '贊助按鈕'}
 ]
