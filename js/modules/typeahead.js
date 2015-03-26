d9dc3207f563623aa9d026ee9ae6ee7a62358f37
Jian Weihang <tonytonyjan@gmail.com>
2015-03-26 16:16:42 +0800

    加速查字速度

--- js/modules/typeahead.js
+++ js/modules/typeahead.js
@@ -6,7 +6,8 @@ var Typeahead = {
       limit: 8,
       remote: {
         url: 'https://tw.search.yahoo.com/sugg/gossip/gossip-tw-vertical_ss/?output=json&pubid=1306&command=%QUERY',
-        filter: function(parsedResponse){ return parsedResponse.gossip.results; }
+        filter: function(parsedResponse){ return parsedResponse.gossip.results; },
+        rateLimitWait: 0
       }
     });
     words.initialize();
