b900085a608ceb4f30c7f6cf5c5d27dda8a13185
Jian Weihang <tonytonyjan@gmail.com>
2015-04-02 21:02:11 +0800

    bump version

--- changelog.js
+++ changelog.js
@@ -1,5 +1,6 @@
 var CHANGELOG = [
   // ex. {title: '修正', message: '「更新日誌」失效連結'},
-  {title: '新增', message: '文字自動補完'},
-  {title: '新增', message: '贊助按鈕'}
+  {title: '新增', message: '開發者日誌'},
+  {title: '異動', message: '美化設定視窗'},
+  {title: '異動', message: '可設定顯示贊助按鈕'}
 ]
