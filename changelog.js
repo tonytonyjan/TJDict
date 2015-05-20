44ff1b38b68e29b8ddebdb698930108f175fb75e
Jian Weihang <tonytonyjan@gmail.com>
2015-05-20 17:55:36 +0800

    bump version

--- changelog.js
+++ changelog.js
@@ -1,5 +1,4 @@
 var CHANGELOG = [
   // ex. {title: '修正', message: '「更新日誌」失效連結'},
-  {title: '新增', message: '江戶小 D 日中字典'},
-  {title: '新增', message: '在 TJDict 頁面也能使用熱鍵'}
+  {title: '修正', message: 'yahoo 字典顯示問題'}
 ]
