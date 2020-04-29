846c73170bb3c85c827e6ed34799a02d716ee53d
Jian Weihang <tonytonyjan@gmail.com>
2020-04-29 11:15:50 +0800

    修正 yahoo 字典

--- js/dictionaries/yahoo.js
+++ js/dictionaries/yahoo.js
@@ -2,10 +2,14 @@ DICTIONARIES.yahoo = {
   title: 'Yahoo 字典',
   desc: '英中、中英',
   langs: ['zh', 'en'],
-  query: function(q, response){
+  query: function (q, response) {
     var self = this;
-    $.get('http://tw.dictionary.search.yahoo.com/search?p=' + q).done(function(data){
-      response(self, $(data).find('.tabsContent'));
-    });
-  }
+    fetch('https://tw.dictionary.search.yahoo.com/search?p=' + q)
+      .then((response) => response.text())
+      .then((body) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(body, 'text/html');
+        response(self, doc.querySelector('.tabsContent-s'));
+      });
+  },
 };
