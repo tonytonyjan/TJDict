51803a30f1a3cbc355ad45ad74bc1e76e98bffeb
Jian Weihang <tonytonyjan@gmail.com>
2020-05-25 16:01:46 +0800

    fix yahoo dictionary

--- src/dictionaries/yahoo.js
+++ src/dictionaries/yahoo.js
@@ -6,12 +6,18 @@ const yahoo = (query) =>
     })
     .then((body) => {
       const dom = new DOMParser().parseFromString(body, "text/html");
-      const result = dom.querySelector(
+      const content = dom.querySelector(
         ".tab-control-item[data-name=explanation]"
       )
         ? dom.querySelector(".tabsContent-s")
         : dom.querySelector(".grp.grp-main");
-      dom
+      if (!content) return null;
+      const kk = dom.querySelector(".compList.d-ib>ul");
+
+      const result = document.createDocumentFragment();
+      if (kk) result.appendChild(kk);
+      result.appendChild(content);
+      result
         .querySelectorAll("[class]")
         .forEach((node) => node.removeAttribute("class"));
       return result;
