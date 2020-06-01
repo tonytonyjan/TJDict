6fca8f6362a10d93d44a889b3d1c9f08c03168ce
Jian Weihang <tonytonyjan@gmail.com>
2020-06-01 16:19:13 +0800

    fix storybook webpack loaders

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,42 @@ module.exports = {
     return {
       ...config,
       resolve: webpackConfig.resolve,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.scss$/i,
+            use: [
+              {
+                loader: "style-loader",
+              },
+              {
+                loader: "css-loader",
+                options: {
+                  importLoaders: 2,
+                },
+              },
+              {
+                loader: "postcss-loader",
+                options: {
+                  ident: "postcss",
+                  postcss: {},
+                  plugins: [
+                    require("postcss-flexbugs-fixes"),
+                    require("autoprefixer")({
+                      flexbox: "no-2009",
+                    }),
+                  ],
+                },
+              },
+              {
+                loader: "sass-loader",
+              },
+            ],
+          },
+        ],
+      },
       plugins: [...config.plugins, ...webpackConfig.plugins],
     };
   },
