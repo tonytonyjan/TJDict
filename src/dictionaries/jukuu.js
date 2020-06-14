485fb996518d31308579ce5e8e0f3d3e9046051f
Jian Weihang <tonytonyjan@gmail.com>
2020-06-14 10:54:28 +0800

    refine jukuu dictionary

--- src/dictionaries/jukuu.js
+++ src/dictionaries/jukuu.js
@@ -1,33 +1,33 @@
 import React from "react";
 
-const jukuu = (query) =>
-  fetch(`http://www.jukuu.com/search.php?q=${encodeURIComponent(query)}`, {
-    referrerPolicy: "no-referrer",
-  })
-    .then((response) => {
-      if (!response.ok) throw new Error("not ok");
-      return response.text();
-    })
-    .then((body) => {
-      const dom = new DOMParser().parseFromString(body, "text/html");
-      var trs1 = dom.querySelectorAll("tr.e > td:nth-child(2)");
-      var trs2 = dom.querySelectorAll("tr.c > td:nth-child(2)");
-      if (trs1.length === 0) return null;
-      return (
-        <ul>
-          {Array.from(trs1).map((i, index) => (
-            <li key={index}>
-              <p dangerouslySetInnerHTML={{ __html: i.innerHTML }}></p>
-              <p
-                style={{ color: "blue" }}
-                dangerouslySetInnerHTML={{ __html: trs2[index].innerHTML }}
-              />
-            </li>
-          ))}
-        </ul>
-      );
-    })
-    .catch(() => null);
+const jukuu = async (query) => {
+  const response = await fetch(
+    `http://www.jukuu.com/search.php?q=${encodeURIComponent(query)}`,
+    {
+      referrerPolicy: "no-referrer",
+    }
+  );
+  if (!response.ok) throw new Error("not ok");
+  const dom = new DOMParser().parseFromString(
+    await response.text(),
+    "text/html"
+  );
+  const trs1 = dom.querySelectorAll("tr.e > td:nth-child(2)");
+  if (trs1.length === 0) return null;
+  const trs2 = dom.querySelectorAll("tr.c > td:nth-child(2)");
+  const pairs = Array.from(trs1).map((v, i) => [v, trs2[i]]);
+  return (
+    <ul>
+      {pairs.map(([t1, t2], index) => (
+        <li key={index}>
+          {t1.textContent}
+          <br />
+          <p className="text-muted">{t2.textContent}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 jukuu.displayName = "句酷";
 
