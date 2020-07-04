5071cf63138d56aec967752bd3e97158f6a6aa74
Jian Weihang <tonytonyjan@gmail.com>
2020-07-04 14:48:29 +0800

    chore: remove unused codes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,3 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpackConfig = require("../webpack.config")(process.env, {});
 module.exports = {
   stories: ["../stories/**/*.stories.js"],
@@ -6,16 +5,10 @@ module.exports = {
     "@storybook/addon-knobs/register",
     "@storybook/addon-actions/register",
   ],
-  webpackFinal: (config) => {
-    require("fs").writeFileSync(
-      "out",
-      require("util").inspect(config, { depth: null })
-    );
-    return {
-      ...config,
-      resolve: webpackConfig.resolve,
-      module: webpackConfig.module,
-      plugins: [...config.plugins, ...webpackConfig.plugins],
-    };
-  },
+  webpackFinal: (config) => ({
+    ...config,
+    resolve: webpackConfig.resolve,
+    module: webpackConfig.module,
+    plugins: [...config.plugins, ...webpackConfig.plugins],
+  }),
 };
